6f4d941e625c2fc8a3a2d5dbd4295211f76286dd
Joey <jvanhoek@hotmail.com>
2018-02-03 14:27:14 +0100

    saving further, loading not working

--- PiCode/RaspNodejsServer/data/SaveLoad.js
+++ PiCode/RaspNodejsServer/data/SaveLoad.js
@@ -18,6 +18,20 @@ app.get('/data', (req, res) => {
     });
 });
 
+app.post('data/load', (req, res) => {
+    console.log(req.body);
+    fs.readFile(path.join(filepath, req.body.filename), (err, data) =>{
+        if (err) {
+            console.log(err.message);
+            res.send(JSON.stringify(err));
+        }
+        else {
+            console.log(data);
+            res.send(data);
+        }
+    });
+});
+
 //todo: parse first and check for errors
 app.post('/data/save', (req, res) => {
     console.log(res.json);
@@ -25,12 +39,12 @@ app.post('/data/save', (req, res) => {
         res.send(JSON.stringify("Empty"));
         return;
     }
-    fs.writeFile(path.join(filepath, 'testfile.json'), "Testing with this string", (err) => {
+    fs.writeFile(path.join(filepath, req.body.filename), "Testing with this string", (err) => {
         if (err) {
             res.sendDate(JSON.stringify(err));
         }
         else {
-            console.log(req.body);
+            console.log(req.body.filename + " :saved");
             res.send(JSON.stringify("File saved"));
         }
     });
